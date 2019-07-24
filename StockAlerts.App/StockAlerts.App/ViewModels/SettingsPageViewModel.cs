8f2538648e780af87fc18d654e179a58f2bc629d
Jon Blankenship <jon@jonblankenship.com>
2019-07-24 06:43:42 -0500

    Finish settings page

--- StockAlerts.App/StockAlerts.App/ViewModels/SettingsPageViewModel.cs
+++ StockAlerts.App/StockAlerts.App/ViewModels/SettingsPageViewModel.cs
@@ -5,6 +5,9 @@ using StockAlerts.App.Services.UserPreferences;
 using StockAlerts.App.ViewModels.Base;
 using StockAlerts.Resources.Model;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -37,7 +40,8 @@ namespace StockAlerts.App.ViewModels
                 if (_userPreferences != null)
                 {
                     _userPreferences.ShouldSendPush = value;
-                    RaisePropertyChanged(nameof(ShouldSendPush));
+                    ErrorMessage = string.Empty;
+                    RaisePropertyChanged();
                 }
             }
         }
@@ -50,7 +54,8 @@ namespace StockAlerts.App.ViewModels
                 if (_userPreferences != null)
                 {
                     _userPreferences.ShouldSendEmail = value;
-                    RaisePropertyChanged(nameof(ShouldSendEmail));
+                    ErrorMessage = string.Empty;
+                    RaisePropertyChanged();
                 }
             }
         }
@@ -63,11 +68,47 @@ namespace StockAlerts.App.ViewModels
                 if (_userPreferences != null)
                 {
                     _userPreferences.ShouldSendSms = value;
-                    RaisePropertyChanged(nameof(ShouldSendSms));
+                    ErrorMessage = string.Empty;
+                    RaisePropertyChanged();
                 }
             }
         }
-        
+
+        public string EmailAddress
+        {
+            get => _userPreferences?.EmailAddress;
+            set
+            {
+                if (_userPreferences != null)
+                {
+                    _userPreferences.EmailAddress = value;
+                    ErrorMessage = string.Empty;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string SmsNumber
+        {
+            get => _userPreferences?.SmsNumber;
+            set
+            {
+                if (_userPreferences != null)
+                {
+                    _userPreferences.SmsNumber =  value;
+                    ErrorMessage = string.Empty;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set => SetProperty(ref _errorMessage, value);
+        }
+
         public async void OnIsActiveChanged(object obj, EventArgs args)
         {
             if (((SettingsPageViewModel)obj).IsActive)
@@ -78,6 +119,8 @@ namespace StockAlerts.App.ViewModels
                 RaisePropertyChanged(nameof(ShouldSendPush));
                 RaisePropertyChanged(nameof(ShouldSendEmail));
                 RaisePropertyChanged(nameof(ShouldSendSms));
+                RaisePropertyChanged(nameof(EmailAddress));
+                RaisePropertyChanged(nameof(SmsNumber));
 
                 IsBusy = false;
             }
@@ -88,6 +131,8 @@ namespace StockAlerts.App.ViewModels
         private async Task ExecuteSaveAsync()
         {
             IsBusy = true;
+
+            _userPreferences.SmsNumber = _userPreferences.SmsNumber.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
 
             var isValid = Validate();
 
@@ -101,6 +146,35 @@ namespace StockAlerts.App.ViewModels
 
         private bool Validate()
         {
+            var errorMessages = new List<string>();
+
+            if (ShouldSendEmail)
+            {
+                if (string.IsNullOrWhiteSpace(EmailAddress))
+                    errorMessages.Add("E-mail address is required.");
+                else
+                {
+                    Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                    Match match = regex.Match(EmailAddress);
+                    if (!match.Success)
+                        errorMessages.Add("E-mail address is not valid.");
+                }
+            }
+
+            if (ShouldSendSms)
+            {
+                if (string.IsNullOrWhiteSpace(SmsNumber))
+                    errorMessages.Add("Phone number is required.");
+                else if (SmsNumber.Length != 10)
+                    errorMessages.Add("Phone number must be 10 digits.");
+            }
+
+            if (errorMessages.Any())
+            {
+                ErrorMessage = string.Join(Environment.NewLine, errorMessages);
+                return false;
+            }
+
             return true;
         }
     }
